da6f9d7b6370b85ff704ce3e4ee314e2b5d0d4fa
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-19 08:28:28 +0700

    req method

--- app.mjs
+++ app.mjs
@@ -3,9 +3,24 @@ import { TodoListService } from "./todolist-service.mjs";
 
 const todoListService = new TodoListService();
 const server = http.createServer((req, res) => {
-    if (req.method === "GET") {
-        todoListService.getTodoList(req, res);
+    res.setHeader("Content-Type", "application/json")
+
+    switch (req.method) {
+        case "GET":
+            todoListService.getTodoList(req, res);
+            break;
+        case "POST":
+            todoListService.createTodoList(req, res);
+            break;
+        case "PUT":
+            todoListService.updateTodoList(req, res);
+            break;
+        case "DELETE":
+            todoListService.deleteTodoList(req, res);
+            break;
+
     }
+
 });
 
 server.listen(3000);
